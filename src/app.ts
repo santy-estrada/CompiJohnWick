b274181e0196f9e1a784f5e0b2f50ea014475150
santy-estrada <santiago.estrada6@eia.edu.co>
2024-04-27 16:49:44 -0500

    Aber

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+//import {router} from "./routes";
+import dbConnect from "./config/mongo";
+
+
+//Set up port for our app
+//If it is not available it would be port 8081
+const PORT = process.env.PORT || 8081;
+//Create an instance of express
+const app = express();
+//cors allows us to make requests to our API
+app.use(cors());
+//config express to use json
+app.use(express.json());
+
+//app.use(router);
+
+//Conectar base de datos
+dbConnect().then(()=>console.log("Conectao"));
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
